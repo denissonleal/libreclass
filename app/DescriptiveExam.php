636b2bb321b0059ca11b2c4089b9907fbd1f4461
denisson leal <denissonleal@gmail.com>
2018-12-02 19:59:00 -0300

    attributes, default values e set attributes do model DescriptiveExam

--- app/DescriptiveExam.php
+++ app/DescriptiveExam.php
@@ -2,11 +2,20 @@
 
 class DescriptiveExam extends \Moloquent
 {
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'attend_id',
+		'exam_id',
+		'description',
+		'approved',
+	];
 
-
-  public function student()
-  {
-    return $this->belongsTo('Attend', 'attend_id')->first()->getUser();
-  }
-
+	public function student()
+	{
+		return $this->belongsTo('Attend', 'attend_id')->first()->getUser();
+	}
 }
