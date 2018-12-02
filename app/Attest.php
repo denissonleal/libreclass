187624cf30309e64f53d1df7faac7b30081c6e13
denisson leal <denissonleal@gmail.com>
2018-12-02 20:19:16 -0300

    attributes, default values e set attributes do model Attest

--- app/Attest.php
+++ app/Attest.php
@@ -1,7 +1,22 @@
 <?php namespace App;
 
-class Attest extends \Moloquent {
+class Attest extends \Moloquent
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'institution_id',
+		'student_id',
+		'date',
+		'days',
+		'description',
+	];
 
-
-
+	public function setDaysAttribute($value)
+	{
+		$this->attributes['days'] = (int) $value;
+	}
 }
