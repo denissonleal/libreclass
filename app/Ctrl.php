8ce06fc541fa8784aa8e7f678db141fd49b0c8ea
denisson leal <denissonleal@gmail.com>
2018-12-02 20:07:49 -0300

    attributes, default values e set attributes do model Ctrl

--- app/Ctrl.php
+++ app/Ctrl.php
@@ -1,5 +1,15 @@
 <?php namespace App;
 
-class Ctrl extends \Moloquent {
-
+class Ctrl extends \Moloquent
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'controller_id', // Id do usuário controlador
+		'subject_id', // Id do usuário controlado
+		'type', // Usuários (ex.: instituição) podem cadastrar usuários (ex.: professor); IP = Instituição controla Professor; ...
+	];
 }
