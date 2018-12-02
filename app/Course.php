ab7093d9b39f5c9a46b004fce8d20b4ba85be460
denisson leal <denissonleal@gmail.com>
2018-12-02 18:31:23 -0300

    attributes, default values e set attributes do model Course

--- app/Course.php
+++ app/Course.php
@@ -3,5 +3,38 @@
 class Course extends \Moloquent {
 
 	public $fillable = [
+		'institution_id',
+		'name',
+		'type', // dados do csv
+		'modality', // dados do csv
+		'absent_percent',
+		'average',
+		'average_final',
+		'status', // E - enable, D - disable
+		'curricular_profile',
 	];
+
+	protected $fillable = [
+		'status' => 'E',
+	];
+
+	public function setNameAttribute($value)
+	{
+		$this->attributes['name'] = titleCase(trimpp($value));
+	}
+
+	public function setAbsentPercentAttribute($value)
+	{
+		$this->attributes['absent_percent'] = (float) $value;
+	}
+
+	public function setAverageAttribute($value)
+	{
+		$this->attributes['average'] = (float) $value;
+	}
+
+	public function setAverageFinalAttribute($value)
+	{
+		$this->attributes['average_final'] = (float) $value;
+	}
 }
