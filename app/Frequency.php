9b1a84de0b26a16dd77cf5fa0c41216b0ab39617
denisson leal <denissonleal@gmail.com>
2018-12-02 19:44:55 -0300

    attributes, default values e set attributes do model Frequency

--- app/Frequency.php
+++ app/Frequency.php
@@ -2,15 +2,29 @@
 
 class Frequency extends \Moloquent
 {
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'offer_id',
+		'user_id',
+		'value',
+	];
 
+	public function setValueAttribute($value)
+	{
+		$this->attributes['value'] = (float) $value;
+	}
 
-  public static function getValue($user, $lesson)
-  {
-    $out = DB::select("select Frequencies.value "
-      . "from Frequencies, Attends "
-      . "where Frequencies.lesson_id=? and Frequencies.attend_id=Attends.id and Attends.user_id=?",
-      [$lesson, $user]);
+	public static function getValue($user, $lesson)
+	{
+		$out = DB::select("select Frequencies.value "
+			. "from Frequencies, Attends "
+			. "where Frequencies.lesson_id=? and Frequencies.attend_id=Attends.id and Attends.user_id=?",
+			[$lesson, $user]);
 
-    return count($out) ? $out[0]->value : "";
-  }
+		return count($out) ? $out[0]->value : "";
+	}
 }
