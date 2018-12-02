e4d68dc4b007164e7c0a6c6cd77d009f9f0e825e
denisson leal <denissonleal@gmail.com>
2018-12-02 19:56:16 -0300

    attributes, default values e set attributes do model Discipline

--- app/Discipline.php
+++ app/Discipline.php
@@ -1,13 +1,40 @@
 <?php namespace App;
 
-class Discipline extends \Moloquent {
-  protected $fillable = ['name', 'period_id'];
+class Discipline extends \Moloquent
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'period_id',
+		'name', // Nome da disciplina
+		'ementa', // ementa da disciplina
+		'status', // E = Enabled; D = Disabled; F = Finalized;
+	];
 
-  public function period() {
-    return $this->belongsTo('Period', 'period_id');
-  }
+	/**
+	 * The model's default values for attributes.
+	 *
+	 * @var array
+	 */
+	protected $attributes = [
+		'status' => 'E',
+	];
 
-  public function getPeriod() {
-    return Period::find($this->period_id);
-  }
+	public function setNameAttribute($value)
+	{
+		$this->attributes['name'] = titleCase(trimpp($value));
+	}
+
+	public function period()
+	{
+		return $this->belongsTo(Period::class);
+	}
+
+	public function getPeriod()
+	{
+		return Period::find($this->period_id);
+	}
 }
