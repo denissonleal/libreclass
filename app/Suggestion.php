d3448f7a74c43f530180d40ab58269fb7a7ae2b4
denisson leal <denissonleal@gmail.com>
2018-12-02 19:03:13 -0300

    attributes, default values e set attributes do model Suggestion

--- app/Suggestion.php
+++ app/Suggestion.php
@@ -1,8 +1,25 @@
 <?php namespace App;
 
-class Suggestion extends \Moloquent {
+class Suggestion extends \Moloquent
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'user_id',
+		'title',
+		'value', // S = sugestões; B = bugs
+		'description',
+	];
 
-
-  public $timestamps = false;
-
+	/**
+	 * The model's default values for attributes.
+	 *
+	 * @var array
+	 */
+	protected $attributes = [
+		'value' => 'S',
+	];
 }
