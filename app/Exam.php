6cca080bf23f8f19929ffdce4198eb6adfaa6a0a
denisson leal <denissonleal@gmail.com>
2018-12-02 18:50:22 -0300

    attributes, default values e set attributes do model Exam

--- app/Exam.php
+++ app/Exam.php
@@ -2,20 +2,50 @@
 
 class Exam extends \Moloquent
 {
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'unit_id',
+		'title',
+		'type', // Tipo de avaliação: E = exams; L = list; P = projects;...
+		'aval', // A = Avaliação, R = Recuperação da Unidade
+		'weight',
+		'date',
+		'comments',
+		'status',
+	];
 
+	/**
+	 * The model's default values for attributes.
+	 *
+	 * @var array
+	 */
+	protected $attributes = [
+		'status' => 'E',
+		'weight' => 1,
+	];
 
-  public function unit()
-  {
-    return $this->belongsTo("Unit", "unit_id");
-  }
+	public function setWeightAttribute($value)
+	{
+		$this->attributes['weight'] = (int) $value;
+	}
 
-  public function descriptive_exams()
-  {
-    $descriptive_exams = $this->hasMany("DescriptiveExam", "exam_id")->get();
-    foreach ($descriptive_exams as $key => $descriptive_exam) {
-      $descriptive_exams[$key]['student'] = $descriptive_exam->student();
-    }
-    return $descriptive_exams;
-  }
+	public function unit()
+	{
+		return $this->belongsTo('Unit', 'unit_id');
+	}
 
+	public function descriptive_exams()
+	{
+		$descriptive_exams = $this->hasMany('DescriptiveExam', 'exam_id')->get();
+
+		foreach ($descriptive_exams as $key => $descriptive_exam) {
+			$descriptive_exams[$key]['student'] = $descriptive_exam->student();
+		}
+
+		return $descriptive_exams;
+	}
 }
