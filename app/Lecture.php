7db5d41f4bdeb77ccc37f04a3187684d4631a4b1
denisson leal <denissonleal@gmail.com>
2018-12-02 19:23:56 -0300

    attributes, default values e set attributes do model Lecture

--- app/Lecture.php
+++ app/Lecture.php
@@ -1,19 +1,44 @@
 <?php namespace App;
 
-class Lecture extends \Moloquent {
+class Lecture extends \Moloquent
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'user_id', // Id do professor que leciona a disciplina
+		'offer_id', // Id da disciplina lecionada
+		'order',
+	];
 
-	protected $fillable = ['user_id', 'offer_id'];
+	/**
+	 * The model's default values for attributes.
+	 *
+	 * @var array
+	 */
+	protected $attributes = [
+		'order' => 1,
+	];
 
-  public function getUser() {
-    return User::find($this->user_id);
-  }
+	public function setOrderAttribute($value)
+	{
+		$this->attributes['order'] = (int) $value;
+	}
 
-  public function getOffer() {
-    return Offer::find($this->offer_id);
-  }
+	public function getUser()
+	{
+		return User::find($this->user_id);
+	}
 
-	public function offer() {
-    return $this->belongsTo('Offer', 'offer_id');
-  }
+	public function getOffer()
+	{
+		return Offer::find($this->offer_id);
+	}
 
+	public function offer()
+	{
+		return $this->belongsTo(Offer::class);
+	}
 }
